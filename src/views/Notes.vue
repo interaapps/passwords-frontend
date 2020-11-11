cccaeadc792952866dd87ba514ba4fa3de8a359d
Julian Gojani <julian@gojani.xyz>
2020-11-11 23:31:11 +0100

    Added Notes-Design

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div id="notes-list">
+        <div id="notes-list" >
             <div id="actions">
                 <svg @click="newNote()" id="add" viewBox="0 0 16 16" class="bi bi-journal-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/><path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/><path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"/></svg>
             </div>
@@ -15,11 +15,12 @@
             <div id="right">
                 <div id="badges">
 
+                    <span v-if="unsaved" style="background: #EE4343" class="badge">UNSAVED</span>
                     <span v-if="!selected" class="badge">NEW</span>
                 </div>
             </div>
-            <input type="text" id="title-input" v-model="title" placeholder="Title">
-            <textarea id="content-input" v-model="contents" placeholder="Just write in here :)"></textarea>
+            <input    @keypress="unsaved = true" type="text" id="title-input" v-model="title" placeholder="Title">
+            <textarea @keypress="unsaved = true" @keydown.ctrl="keyboardEvents" id="content-input" v-model="contents" placeholder="Just write in here :)"></textarea>
         </div>
     </div>
 </template>
@@ -28,18 +29,21 @@ export default {
     data: ()=>({
         selected: null,
         title: "",
-        contents: ""
+        contents: "",
+        unsaved: false
     }),
     methods:{
         selectNote(note){
             this.selected = note.id
             this.title    = note.title
             this.contents = note.content
+            this.unsaved  = false
         },
         newNote(){
             this.selected = null
             this.title    = ""
             this.contents = ""
+            this.unsaved  = true
         },
         save(){
             this.passwordsClient
@@ -47,9 +51,17 @@ export default {
 
                 }).then(res=>{
                     if (res.success) {
-                        this.selected = res.extra.id
+                        if (!res.extra.updated)
+                            this.selected = res.extra.id
+                        this.unsaved  = true
                     }
                 })
+        },
+        keyboardEvents(e){
+            if (e.code == 'KeyS'){
+                console.log("SAVING");
+                e.preventDefault()
+            }
         }
     }
 }
