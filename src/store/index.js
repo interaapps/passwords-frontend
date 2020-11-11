3f3fc31f8585f660d0bfe22c2fcc4e6ef680943c
Julian Gojani <julian@gojani.xyz>
2020-11-11 23:10:15 +0100

    Added Notes-Design

--- src/store/index.js
+++ src/store/index.js
@@ -7,47 +7,33 @@ export default new Vuex.Store({
   state: {
     fetched: false,
     user: {
-      picture: 'https://cdn.interaapps.de/service/accounts/profilepics/53_12432e8f906dbc82fd8368a7ccf5aa2e0ceca6fc2ab0bb53c7f3efeb865ff44941c08bd7365b4d7f7f63a7a9642d1ba07f439fe08ecfecebb6caebd0e5071902676418d7464c3967c770375326d90a1b.png.png',
     },
     encryptionKey: null,
     currentPassword: null,
     masterPassword: null, // The encrypted encryption-key. Null if not exists
     currentFolder: null,
+    notes: [
+        {
+          id: 1,
+          title: "Hello world",
+          content: "Yes it works",
+          updated_at: "11.9.2001"
+        },
+        {
+          id: 2,
+          title: "Second",
+          content: "Yes it works a second time asfdasdf asdf ",
+          updated_at: "11.9.2001"
+        }
+    ],
     passwords: {
       root: true,
       folders: {
         1: {
           id: 1,
           folder: {
-            name: "New Folder",
+            name: "LOADING",
             color: "#EA5555",
-          },
-          folders: {
-            3: {
-              id: 3,
-              folder: {
-                name: "Test",
-                color: "#586622"
-              }
-            },
-          },
-          passwords: {
-            44: {
-              id: 44,
-              name: "YEA, Geschafft!",
-              username: "Test@interaapps.de",
-              password: "test",
-              website: "https://accounts.google.com",
-              description: `Test`,
-            }
-          },
-          parent: 0
-        },
-        2: {
-          id: 2,
-          folder: {
-            name: "Test",
-            color: "#58EA55",
           },
           parent: 0
         }
@@ -55,11 +41,11 @@ export default new Vuex.Store({
       passwords: {
         34: {
           id: 34,
-          name: "Twitter login",
-          username: "Test@interaapps.de",
-          password: "test",
-          website: "https://twitter.com/login",
-          description: `Test`
+          name: "LOADING",
+          username: "LOADING",
+          password: "LOADING",
+          website: "LOADING",
+          description: `LOADING`
         }
       },
       parent: null
--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -5,20 +5,32 @@
                 <svg id="add" viewBox="0 0 16 16" class="bi bi-journal-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/><path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/><path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"/></svg>
             </div>
             <div id="list">
-                <div class="entry">
-                    <h1>New Note</h1>
-                    <h2>New Note</h2>
+                <div class="entry" v-for="note in $store.state.notes" :key="note.id" @click="selectNote(note)" :class="{selected: selected == note.id}">
+                    <h1>{{note.title}}</h1>
+                    <h2>{{note.content.substring(0, 35)}}</h2>
                 </div>
             </div>
         </div>
         <div id="current-note">
-
+            <input type="text" id="title-input" v-model="title" placeholder="Title">
+            <textarea id="content-input" v-model="contents" placeholder="Just write in here :)"></textarea>
         </div>
     </div>
 </template>
 <script>
 export default {
-    
+    data: ()=>({
+        selected: null,
+        title: "",
+        contents: ""
+    }),
+    methods:{
+        selectNote(note){
+            this.selected = note.id
+            this.title    = note.title
+            this.contents = note.content
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
@@ -45,7 +57,6 @@ export default {
 
     #list {
         .entry {
-            background: #00000005;
             padding: 10px 14px;
             cursor: pointer;
             h1 {
@@ -62,9 +73,46 @@ export default {
                 width: 100%;
                 overflow: hidden;
                 text-overflow: ellipsis;
+                white-space: nowrap;
                 color: #545454;
             }
+
+            &.selected {
+                background: #00000006;
+            }
+
+            &:hover {
+                background: #00000004;
+            }
         }
+    }
+}
+
+#current-note {
+    margin-left: 300px;
+    margin-top:  110px;
+    display: block;
+    height: 100%;
+
+    input,
+    textarea {
+        border: none;
+        outline: none;
+        display: block;
+    }
+
+    #title-input {
+        width: 100%;
+        margin-bottom: 40px;
+        font-size: 40px;
+        font-weight: 600;
+    }
+
+    #content-input {
+        width: 100%;
+        font-size: 24px;
+        min-height: calc(100vh - 200px);
+        resize: vertical;
     }
 }
 </style>
