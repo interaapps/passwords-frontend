8bdc1d3925f73ec02fdbd75434e5785097c2c4da
Julian Gojani <julian@gojani.xyz>
2020-11-13 23:36:43 +0100

    Made notes more mobile

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -148,6 +148,7 @@ export default {
                 width: 100%;
                 overflow: hidden;
                 text-overflow: ellipsis;
+                white-space: nowrap;
                 color: #434343;
             }
             h2 {
